d7b35af4af9005808e84890d09a2eb26ad58d2c7
CutThroat <55604778+cutthroat78@users.noreply.github.com>
2022-08-20 21:07:28 +0100

    Initial commit from 01/06/2022

---
+++ Just Rick Roll (Self Destruct).bat
@@ -0,0 +1,102 @@
+@echo off
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+start /b "" cmd /c del "%~f0"&exit /b
+
---
+++ Just Rick Roll.bat
@@ -0,0 +1,100 @@
+@echo off
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
+ping 0.0.0.0 > NUL
+start https://youtu.be/dQw4w9WgXcQ
